06705efc3d9050d0e8bdb5d6d07e69bcaa1870fe
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2023-10-12 12:24:40 +0200

    Validation of form fields

--- frontend/src/components/TestComponent.vue
+++ frontend/src/components/TestComponent.vue
@@ -2,10 +2,14 @@
   <v-card class="box" variant="outlined">
     <v-card-title>Test Component</v-card-title>
     <v-card-text>
-      Body
+      <v-form v-model="isPersonValid">
+        <v-text-field variant="solo" label="First name" v-model="person.firstName" :rules="[ rules.required ]"></v-text-field>
+        <v-text-field variant="solo" label="Last name" v-model="person.lastName" :rules="[ rules.required ]"></v-text-field>
+        <v-text-field variant="solo" label="Year of birth" type="number" v-model="person.yearOfBirth" :rules="[ rules.required, rules.fourDigits ]"></v-text-field>
+      </v-form>
     </v-card-text>
     <v-card-actions class="actions">
-      <v-btn variant="elevated" color="primary" @click="action">Action</v-btn>
+      <v-btn variant="elevated" color="primary" @click="action" :disabled="!isPersonValid">Action</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -16,9 +20,24 @@ export default {
   mounted: () => console.log('TestComponent mounted'),
   methods: {
     action() {
-      console.log('Action was performed')
+      this.person.yearOfBirth = parseInt(this.person.yearOfBirth)
+      console.log('Action was performed on person', this.person)
     }
-  }
+  },
+  data() {
+    return {
+      isPersonValid: false,
+      rules: {
+        required: value => !!value || 'empty value is not allowed',
+        fourDigits: value => /^\d\d\d\d$/.test(value) || 'enter four digits'
+      },
+      person: {
+        firstName: '',
+        lastName: '',
+        yearOfBirth: 1900
+      }     
+    }
+  } 
 }
 </script>
 
