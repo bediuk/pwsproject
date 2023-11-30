821b15e92cdafd186ff27f92fb5da62db5b4c8a4
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2023-11-30 11:46:31 +0100

    Authorization UI (very simple)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,6 +11,16 @@
 
       <v-divider></v-divider>
 
+      <div>{{ user.username || 'not-logged-in' }}</div>
+      <div v-if="!user.username">
+        <v-text-field variant="solo" label="Username" v-model="creds.username"></v-text-field>
+        <v-text-field variant="solo" type="password" label="Password" v-model="creds.password"></v-text-field>
+        <v-btn @click="login">Login</v-btn>
+      </div>
+      <div v-if="user.username">
+        <v-btn @click="logout">Logout</v-btn>
+      </div>
+
     </v-navigation-drawer>
 
     <v-main>
@@ -29,8 +39,36 @@ export default {
           { title: 'Dashboard', icon: 'mdi-view-dashboard', href: '#/' },
           { title: 'Persons', icon: 'mdi-account-multiple', href: '#/persons' },
           { title: 'Projects', icon: 'mdi-sitemap-outline', href: '#/projects' }
-      ]
+      ],
+      user: {},
+      creds: {}
     }
+  },
+  methods: {
+    logout() {
+      fetch('/auth', { method: 'DELETE' })
+      .then(res => res.json())
+      .then(data => {
+        this.user = data
+      })
+    },
+    login() {
+      fetch('/auth', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(this.creds) })
+      .then(res => res.json())
+      .then(data => {
+        this.user = data
+      })
+    }
+  },
+  mounted() {
+    fetch('/auth', { method: 'GET' })
+    .then(res => res.json())
+    .then(data => {
+      this.user = data
+    })
   }
 }
 </script>
