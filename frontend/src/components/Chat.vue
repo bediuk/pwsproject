025ce6a9b46c1b4126239e2c66a8f866e15f67ff
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2023-12-14 09:56:51 +0100

    Chat

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -2,7 +2,20 @@
     <v-card variant="text">
         <v-card-title>Chat</v-card-title>
         <v-card-text>
+            <v-list>
+                <v-list-item v-for="(data, index) in history" :key="index"
+                :subtitle="data.timestamp.toLocaleTimeString()">
+                    {{ data.message }}
+                </v-list-item>
+            </v-list>
         </v-card-text>
+        <v-card-actions>
+            <v-text-field variant="solo" label="Message" v-model="message">
+                <template #append-inner>
+                    <v-btn variant="elevated" color="success" @click="send">Send</v-btn>
+                </template>
+            </v-text-field>
+        </v-card-actions>
     </v-card>
 </template>
   
@@ -12,17 +25,37 @@ export default {
     props: [ 'user' ],
     data() {
         return {
-            connection: null
+            connection: null,
+            message: '',
+            history: []
+        }
+    },
+    methods: {
+        send() {
+            this.connection.send(JSON.stringify({ event: 'CHAT', message: this.message }))
+            this.message = ''
         }
     },
     mounted() {
         this.connection = new WebSocket('ws://' + window.location.host + '/websocket')
         this.connection.onopen = () => {
             console.log('Websocket connection established')
-            this.connection.send('Now is ' + new Date())
         }
         this.connection.onmessage = (event) => {
-            console.log('Websocket is getting message', event.data)
+            let data = {}
+            try {
+                data = JSON.parse(event.data)
+                data.timestamp = new Date(data.timestamp)
+            } catch(err) {
+                console.error('Broken WS data', event.data)
+                return
+            }
+            console.log('WS data arrived',)
+            switch(data.event) {
+                case 'CHAT':
+                    this.history.push(data)
+                    break
+            }
        }
     }    
 }
--- websocket.js
+++ websocket.js
@@ -1,7 +1,16 @@
 module.exports = wsInstance => (ws, req) => {
     ws.on('message', rawData => {
+        let data = {}
+        try {
+            data = JSON.parse(rawData)
+        } catch(err) {
+            console.error('WS error:', err.message)
+            return
+        }
+        data.timestamp = new Date()
+        console.log('WS data:', data)
         wsInstance.getWss().clients.forEach(client => {
-            client.send(rawData.toUpperCase())
+            client.send(JSON.stringify(data))
         })
     })
 }
